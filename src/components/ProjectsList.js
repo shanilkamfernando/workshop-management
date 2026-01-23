504c1a41526114d05a4b47949771d10a04d5257f
shanilkamfernando <shanilkamfernando123@gmail.com>
2026-01-23 23:11:38 +0530

    updated

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -210,9 +210,21 @@ function ProjectsList() {
           <div className="flex items-center space-x-3">
             <button
               onClick={() => navigate("/partners")}
-              className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg flex items-center space-x-2 transition"
+              className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded-lg transition-colors duration-200 flex items-center space-x-2"
             >
-              <span>Back</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <span>Back to Partners</span>
             </button>
 
             <button
